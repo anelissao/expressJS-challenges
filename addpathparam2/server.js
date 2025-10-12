b914b6a4eb78da507af89b35896cf5c414c85eaf
anelissao <anouarelissaoui20@gmail.com>
2025-10-12 20:41:05 +0100

    generating status code

--- addpathparam2/server.js
+++ addpathparam2/server.js
@@ -45,14 +45,55 @@ app.get('/api', (req, res) => {
 
 })
 
+
 app.get('/api/:field/:term', (req, res) => {
+  
   const { field, term } = req.params
+    
+  const allowedFields = ['country', 'continent', 'industry']
+
+
+  if (!allowedFields.includes(field)) {
+    return res.status(400).json({message: "Search field not allowed. Please use only 'country', 'continent', 'industry'" })
+  }
+
+
+/*
+Challenge:
+1. If the client’s 'field' is not supported, serve this object:
+  {message: "Search field not allowed. Please use only 'country', 'continent', 'industry'" }
+2. Chain in the .status(<code>) method to set a status code.
+	What status code should you set?
+3. You might run into an error! Find a solution!
+
+hint.md for help!
+*/
 
   const filteredData = startups.filter(
-	startup => startup[field].toLowerCase() === term.toLowerCase()
+    startup => startup[field].toLowerCase() === term.toLowerCase()
   )
+
   res.json(filteredData)
+
 })
+
+/*
+** The functionality **
+Get all startups in a given country via api/country/<country name>
+Get all startups in a given continent via api/continent/<continent name>
+Get all startups in a given industry via api/industry/<industry name>
+
+**Test Cases** 
+
+These should work:
+  api/country/india
+  api/continent/europe
+  api/industry/ai
+
+This should return the object given in the challenge above. 
+	api/has_mvp/true
+
+*/
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
 

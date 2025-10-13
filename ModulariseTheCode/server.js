85dfb176084aa9ee71b4f48d3952a5e0ab573cfb
anelissao <anouarelissaoui20@gmail.com>
2025-10-13 10:06:31 +0100

    modularizing the code

---
+++ ModulariseTheCode/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { apiRouter } from './routes/apiRoutes.js'
+
+const PORT = 8000
+
+const app = express()
+
+app.use('/api', apiRouter)
+
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
+
